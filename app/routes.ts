85fdd4733edaa7d46baaf0580233cb91ef32e470
FusedBaggins <faguiar408@gmail.com>
2021-11-21 22:08:29 -0300

    feat: register user's routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
+import User from "./controllers/user.controller";
 const routes = Router();
+
+
+routes.get("/users", User.list);
+routes.get("/users/:id", User.detail);
+routes.patch("/users/:id", User.patch);
 
 
 export default routes;
