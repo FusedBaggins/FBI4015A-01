ba408759e606ff4d99153d8e7676421319dddf03
FusedBaggins <faguiar408@gmail.com>
2021-12-05 13:36:03 -0300

    fix: change upload file name

--- app/routes.ts
+++ app/routes.ts
@@ -9,7 +9,7 @@ const routes = Router();
 
 routes.get("/users", User.list);
 routes.get("/users/:id", User.detail);
-routes.patch("/users/:id", upload.single("profile"), User.patch);
+routes.patch("/users/:id", upload.single("profilePhoto"), User.patch);
 
 routes.get("/payment-methods", paymentMethods.list);
 routes.post("/payment-methods", paymentMethods.create);
